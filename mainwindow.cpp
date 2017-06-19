447b2882416698793301163744c209689fe32f00
hiloshi.syu <hiloshi.syu@gmail.com>
2017-06-19 23:28:25 +0800

    CPU usage increase to 100%

---
+++ mainwindow.cpp
@@ -0,0 +1,83 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    serial = new QSerialPort(this);
+    connect(serial, &QSerialPort::readyRead, this, &MainWindow::UART_Receiver2);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QByteArray data("ttyUSB0 Open OK \r\n");
+
+    serial->setPortName("ttyUSB0");
+    serial->setBaudRate(QSerialPort::Baud9600);
+    serial->setDataBits(QSerialPort::Data8);
+    serial->setParity(QSerialPort::NoParity);
+    serial->setStopBits(QSerialPort::OneStop);
+    serial->setFlowControl(QSerialPort::NoFlowControl);
+    if (serial->open(QIODevice::ReadWrite))
+    {
+        qDebug()<<"UART Open OK ";
+        serial->write(data);
+    }
+    else
+        qDebug()<<"Open UART Port Fail ";
+    //----------------------------------------
+    MySeries = new QSplineSeries();
+    MySeries->setName("QChart");
+    Mychart = new QChart();
+    Mychart->addSeries(MySeries);
+    Mychart->setTitle("Display");
+    Mychart->createDefaultAxes();
+    Mychart->axisY()->setRange(0,15000);
+    Mychart->axisX()->setRange(0,100);
+    //PM25chart->setAnimationOptions(QChart::SeriesAnimations);
+
+    MychartView = new QChartView(Mychart);
+    //PM25chartView->setRenderHint(QPainter::Antialiasing);
+
+    Mywidget = new QWidget();
+    MyvLayout = new QVBoxLayout();
+    MyvLayout->addWidget(MychartView);
+    Mywidget->setLayout(MyvLayout);
+    ui->tabWidget->addTab(Mywidget,"Chart");
+    My_chart_max =100;
+    elapse_time =0;
+
+    My_x=My_y=My_y_max=0;
+    MyData.clear();
+
+    for(int i=0;i<My_chart_max;i++)
+        MySeries->append(i, i);
+}
+
+void MainWindow::UART_Receiver2()
+{
+
+    UART_Read_Buffer = UART_Read_Buffer + serial->readAll().toHex();
+
+    if(UART_Read_Buffer.count()>63)
+    {
+
+    //--------------------------------------------                      //count number of input data. .byJC
+        elapse_time++;
+        QString s = QString::number(elapse_time);                       //convert int to string. . byJC
+        Mychart->axisX()->setTitleText(s);
+        //qDebug()<<"count="<<UART_Read_Buffer.count();
+    //-------------------------------------------
+        UART_Read_Buffer.clear();
+    }
+
+}
---
+++ mainwindow.h
@@ -0,0 +1,48 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtSerialPort/QSerialPort>
+#include <QSplineSeries>
+#include <QChart>
+#include <QChartView>
+#include <QVBoxLayout>
+
+using namespace QtCharts;
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void UART_Receiver2();
+
+    void on_pushButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QSerialPort     *serial;
+    QByteArray      UART_Read_Buffer;                        //UART Read Buffer
+  //---------------------------------------------------------
+    QSplineSeries   *MySeries;
+    QChart          *Mychart;
+    QChartView      *MychartView;
+    QWidget         *Mywidget;
+    QVBoxLayout     *MyvLayout;
+    int             My_x;
+    int             My_y,My_y_max;
+    QList           <int>MyData;
+    int             My_chart_max;
+    int             elapse_time;
+
+};
+
+#endif // MAINWINDOW_H
